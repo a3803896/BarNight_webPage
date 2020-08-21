bb7f38e0a9ebcbe82f715278bc07d18f3d344c17
Alex Wang <a3803896@gmail.com>
2020-08-21 21:26:02 +0800

    修改首頁

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="container navBar position-relative">
-        <nav class="d-flex justify-content-center justify-content-lg-between align-items-center pt-2 pb-2">
-            <i class="material-icons menuIcon d-block d-lg-none text-white" @click="showList">menu</i>
-            <h1 class="d-flex align-items-center"><router-link to="/homepage" class="h2 text-main marker">Bar Night</router-link></h1>
-            <ul class="navList list-unstyled d-flex flex-column flex-lg-row m-0 align-items-center">
-              <li><router-link to="/about" class="link noto">關於我們</router-link></li>
-              <li><router-link to="/products" class="link noto">產品列表</router-link></li>
-              <li><router-link to="/cart" class="link noto">購物車</router-link></li>
-              <li><router-link to="/check" class="link noto">結帳</router-link></li>
-              <!-- <li><router-link to="/login" class="text-danger noto">登入後台</router-link></li> -->
-            </ul>
-        </nav>
+    <div class="navBar position-relative">
+        <div class="container">
+          <nav class="d-flex justify-content-center justify-content-lg-between align-items-center pt-2 pb-2">
+              <i class="material-icons menuIcon d-block d-lg-none text-white" @click="showList">menu</i>
+              <h1 class="d-flex align-items-center"><router-link to="/homepage" class="text-main marker">Bar Night</router-link></h1>
+              <ul class="navList list-unstyled d-flex flex-column flex-lg-row m-0 align-items-center">
+                <li><router-link to="/about" class="link noto">關於我們</router-link></li>
+                <li><router-link to="/products" class="link noto">產品列表</router-link></li>
+                <li><router-link to="/cart" class="link noto">購物車</router-link></li>
+                <li><router-link to="/check" class="link noto">結帳</router-link></li>
+                <!-- <li><router-link to="/login" class="text-danger noto">登入後台</router-link></li> -->
+              </ul>
+          </nav>
+        </div>
       </div>
 </template>
 
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -21,50 +21,49 @@
             <div class="mb-5 row d-flex justify-content-lg-center">
                 <div class="col-12 col-lg-12 d-md-flex flex-md-wrap justify-content-md-between">
                     <div class="card border-0 mb-4 flex-lg-row">
-                        <img src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/25eKyhOzx1VaMJiIUi53tIKByFUdG1JVcEHNfpJUMTUNXE9dmUzFuilwbripHTw8Ae9noCCcKKtfZSGBGAinSBzE1Dn3RRIGIWVp1yyacXK5xjvWPYUmgBRTIIS3qHH8.jpg" class="clippy-reverse card-img-top position-90" alt="">
+                        <img src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/02r2dZonYIGObMXF6MbCPvMqdq1BOAZuNBQLWxa9Hf6ey3ntUI8GmNRtss7Kx0KEMnYpZrAO6Dx98tiG4t0Bg9VAI9jdeBLndcyFaejCipTEdMAgRErwawRSernYXiFw.jpg" class="clippy-reverse card-img-top position-60 trans" alt="">
                         <div class="card-body pl-0 p-lg-3 d-lg-flex flex-column justify-content-lg-center align-items-lg-center">
-                            <h5 class="card-title text-white bold">Card title</h5>
-                            <p class="card-text text-white bold w-75">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h3 class="card-text text-white bold w-75 text-left noto">專業調酒師為您調製多樣化的調酒，酸甜輕重 隨你所欲。</h3>
                         </div>
                     </div>
                     <div class="card border-0 mb-4 flex-lg-row-reverse">
-                        <img src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/7cDf3BOgONHWeAK9hfgvgGt3FJB7uFTQ4Mq2TGaJd0a1Gg19IMtkUppHuETagTBcPjj0gNzdHrokHdhIdmg3xLOWXqe227VRcgWvcHtjsgRYefyE7sG3uVA8GuVjqAHo.jpg" class="clippy card-img-top position-74" alt="">
+                        <img src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/7cDf3BOgONHWeAK9hfgvgGt3FJB7uFTQ4Mq2TGaJd0a1Gg19IMtkUppHuETagTBcPjj0gNzdHrokHdhIdmg3xLOWXqe227VRcgWvcHtjsgRYefyE7sG3uVA8GuVjqAHo.jpg" class="clippy card-img-top position-74 trans" alt="">
                         <div class="card-body pl-0 p-lg-3 d-lg-flex flex-column justify-content-lg-center align-items-lg-center">
-                            <h5 class="card-title text-white bold">Card title</h5>
-                            <p class="card-text text-white bold w-75">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h3 class="card-text text-white bold w-75 noto text-left">每日採買新鮮食材及調酒原料，讓您喝得開心、也喝得安心。</h3>
                         </div>
                     </div>
                     <div class="card border-0 mb-4 flex-lg-row">
-                        <img src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/02r2dZonYIGObMXF6MbCPvMqdq1BOAZuNBQLWxa9Hf6ey3ntUI8GmNRtss7Kx0KEMnYpZrAO6Dx98tiG4t0Bg9VAI9jdeBLndcyFaejCipTEdMAgRErwawRSernYXiFw.jpg" class="clippy-reverse card-img-top position-60" alt="">
+                        <img src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/25eKyhOzx1VaMJiIUi53tIKByFUdG1JVcEHNfpJUMTUNXE9dmUzFuilwbripHTw8Ae9noCCcKKtfZSGBGAinSBzE1Dn3RRIGIWVp1yyacXK5xjvWPYUmgBRTIIS3qHH8.jpg" class="clippy-reverse card-img-top position-90 trans" alt="">
                         <div class="card-body pl-0 p-lg-3 d-lg-flex flex-column justify-content-lg-center align-items-lg-center">
-                            <h5 class="card-title text-white bold">Card title</h5>
-                            <p class="card-text text-white bold w-75">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h3 class="card-text text-white bold w-75 noto text-left">全店使用 3M 廚下淨水系統，從飲用水到冰塊皆純淨無瑕。</h3>
                         </div>
                     </div>
                     <div class="card border-0 flex-lg-row-reverse">
-                        <img src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/6brtREw0qo7dt4yOij0cPVCGcCLRfH43kSUWXmx3TGs7v0FJUflbNN9VJDZsULErWOSmGu56YngFB6vS7GSZlBXFX225PNL02Zi5M36BvfSUUAIBkUj2VGpRrB6VQjok.jpg" class="clippy card-img-top" alt="">
-                        <div class="card-body pl-0 p-lg-3 d-lg-flex flex-column justify-content-lg-center align-items-lg-center">
-                            <h5 class="card-title text-white bold">Card title</h5>
-                            <p class="card-text text-white bold w-75">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <img src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/6brtREw0qo7dt4yOij0cPVCGcCLRfH43kSUWXmx3TGs7v0FJUflbNN9VJDZsULErWOSmGu56YngFB6vS7GSZlBXFX225PNL02Zi5M36BvfSUUAIBkUj2VGpRrB6VQjok.jpg" class="clippy card-img-top trans" alt="">
+                         <div class="card-body pl-0 p-lg-3 d-lg-flex flex-column justify-content-lg-center align-items-lg-center">
+                            <h3 class="card-text text-white bold w-75 noto text-left">最頂級的食材配上最頂尖的手藝，多道美味的餐點任君挑選。</h3>
                         </div>
                     </div>
                 </div>
             </div>
         </main>
-        <div class="container-fluid mb-5">
-            <div class="row">
-                <div class="col-12 no-p15">
-                    <div class="bartender pl-lg-5 p-3 d-flex flex-column justify-content-between position-relative">
-                        <h3 class="text-white z-10 noto text-right">「沒有人的溫暖，</h3>
-                        <h3 class="text-white z-10 noto m-0 vertical align-self-end">就稱不上是酒吧。」</h3>
-                        <div class="pl-lg-5">
-                            <p class="text-white z-10 noto">本店特聘、2019年台灣十大調酒師</p>
-                            <h2 class="marker text-main z-10 bold">JESSICA WANG</h2>
+        <div class="white-texture">
+            <div class="container pt-5 pb-5">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="bartender pl-lg-5 p-3 d-flex flex-column justify-content-between position-relative">
+                            <h3 class="text-white z-10 noto text-right">「沒有人的溫暖，</h3>
+                            <h3 class="text-white z-10 noto m-0 vertical align-self-end">就稱不上是酒吧。」</h3>
+                            <div class="pl-lg-5">
+                                <p class="text-white z-10 noto">本店監製：2019年台灣十大調酒師</p>
+                                <h2 class="marker text-main z-10 bold">JESSICA WANG</h2>
+                            </div>
+                            <div class="bartenderOpacity position-absolute"></div>
                         </div>
-                        <div class="bartenderOpacity position-absolute"></div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 </template>
